284f5e53b4dee18bf8342813ed87062f1d376e79
Cleef Souza <a.cleef.souza@gmail.com>
2026-02-15 18:03:43 -0300

    [Feat][Refact][#4]: Alterando conceito, nomenclatura e proposito do mod

--- src/main/java/br/pepola/mod/manager/CommandCenter.java
+++ src/main/java/br/pepola/mod/manager/CommandCenter.java
@@ -2,25 +2,23 @@ package br.pepola.mod.manager;
 
 import br.pepola.mod.commands.ListEffectsCommand;
 import br.pepola.mod.commands.ListVFXCommand;
-import br.pepola.mod.commands.OrbCommandOff;
-import br.pepola.mod.commands.OrbCommandOn;
+import br.pepola.mod.commands.LumosCommandOff;
+import br.pepola.mod.commands.LumosCommandOn;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractCommandCollection;
 
 public class CommandCenter extends AbstractCommandCollection {
 
-    private final String SUB_LOGGER = "Command Center";
+    private final String SUB_LOGGER = "Central";
     private final HytaleLogger logger;
-    private final OrbManager manager;
 
-    public CommandCenter(HytaleLogger logger, OrbManager manager) {
-        super("orb", "Command center of plugin");
+    public CommandCenter(HytaleLogger logger, Manager manager) {
+        super("orb", "Central de comandos do plugin");
 
-        this.manager = manager;
         this.logger = logger.getSubLogger(SUB_LOGGER);
 
-        this.addSubCommand(new OrbCommandOn(logger, manager));
-        this.addSubCommand(new OrbCommandOff(logger, manager));
+        this.addSubCommand(new LumosCommandOn(logger, manager));
+        this.addSubCommand(new LumosCommandOff(logger, manager));
         this.addSubCommand(new ListEffectsCommand(logger));
         this.addSubCommand(new ListVFXCommand(logger));
     }
